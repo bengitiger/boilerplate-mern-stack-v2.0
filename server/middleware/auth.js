f0aef5d07a1ca65ddcfcaed75b81746eceed4b2e
John Ahn <smileajw1004@gmail.com>
2020-05-04 13:47:29 +0900

    Fix

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,5 @@
 const moment = require('moment');
-const { User } = require('../models/user');
+const { User } = require('../models/User');
 
 let auth = (req, res, next) => {
   const token = req.header('x_token');
